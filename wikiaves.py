0c6a3f90fc649dae4cd8a14d773b266010d2f9f1
Fernando Ferreira <fernandoferreira@usp.br>
2025-04-21 20:15:23 -0300

    Refactoring to correct errors when using xpath.gitignore

--- wikiaves.py
+++ wikiaves.py
@@ -7,13 +7,14 @@ from datetime import datetime
 import pandas as pd
 
 id_min = 1
-id_max = 8#6802457
+id_max = 20#86803661
 
 
 headers = {
     'User-Agent': 'Mozilla/5.0',
     'Accept-Language': 'pt-BR,pt;q=0.5'
 }
+
 id = []
 nome_comum = []
 nome_cientifico = []
@@ -24,27 +25,44 @@ for i in range(id_min, id_max):
     url = f"https://www.wikiaves.com.br/{i}"
     res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.content, "html.parser")
-
     dom = etree.HTML(str(soup))
     if dom.xpath('/html/body/div/div[1]/div/div[1]/div[2]/text()'):
         pass
     else:
         id.append(i)
-        nome_comum.append(dom.xpath('/html/body/div[1]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/p/a[1]/text()')[0])
-        nome_cientifico.append(dom.xpath('/html/body/div[1]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/p/a[3]/i[1]/text()')[0])
-        municipio.append(dom.xpath('/html/body/div[1]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[7]/div[2]/div/a[1]/text()')[0])
-        cod_mun = dom.xpath('/html/body/div[1]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[7]/div[2]/div/a')[0]
-        if '=' in cod_mun.get('href'):
-            codigo = cod_mun.get('href').split('=')[1]
-        elif '_' in cod_mun.get('href'):
-            codigo = cod_mun.get('href').split('_')[1]
+        for div in soup.find_all('div', class_='wa-lista-detalhes'):
+            paragraph = div.find('p')
+            a_tag = paragraph.find('a')
+            if a_tag:
+                nome_comum.append(a_tag.text.strip())
+            i_tag = paragraph.find('i')
+            if i_tag:
+                nome_cientifico.append(i_tag.text.strip())
+        for div in soup.find_all('div'):
+            label = div.find('label')
+            if label and label.get_text(strip=True) == 'Município:':
+                a_tag = div.find('a')
+                if a_tag:
+                    mun = a_tag.text.strip()
+                    cod_mun = a_tag.get('href')
+        municipio.append(mun)
+        if '=' in cod_mun:
+            codigo = cod_mun.split('=')[1]
+        elif '_' in cod_mun:
+            codigo = cod_mun.split('_')[1]
         cod_municipio.append(codigo)
-        data_registro.append(dom.xpath('/html/body/div[1]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[8]/text()')[0])
+        for div in soup.find_all('div'):
+            label = div.find('label')
+            if label and ((label.get_text(strip=True) == 'Feita em:') or (label.get_text(strip=True) == 'Gravado em:')):
+                contents = label.find_all_next(string=True)
+                data_registro.append(contents[1])
+
 dados = pd.DataFrame({'id': id,
                       'nome comum': nome_comum,
                       'nome cientfico': nome_cientifico,
                       'município': municipio,
                       'código municipio': cod_municipio,
-                      'data do regristro': data_registro})
+                      'data do registro': data_registro})
+dados['data do registro'] = pd.to_datetime(dados['data do registro'])
 dados.to_csv('dados.csv', index=False)
 
